65003d7816a84eb0f1a0f3b32efad194cd8d2ef7
thuutri <thuutri2710@gmail.com>
2021-04-08 01:38:51 +0700

    final deploy

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :backend, ScmWeb.Endpoint,
   load_from_system_env: true
 
 # Do not print debug messages in production
-config :logger, level: :info, :ssl
+config :logger, level: :info
 
 # ## SSL Support
 #
