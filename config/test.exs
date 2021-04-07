d8dbee79de945dae71f88096d37a9e2aa6657984
Team Dwarves <ngotrongkhoi112@gmail.com>
2021-04-07 18:29:07 +0700

    feat: ci/cd

--- config/test.exs
+++ config/test.exs
@@ -16,7 +16,7 @@ config :scm, Scm.Repo,
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :scm, ScmWeb.Endpoint,
-  http: [port: 4002],
+  http: [port: 4000],
   server: false
 
 # Print only warnings and errors during test
