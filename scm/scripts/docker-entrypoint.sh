826685d3d692f4aed10077329fb0b8c0a22b4ec1
thuutri <thuutri2710@gmail.com>
2021-04-07 17:46:57 +0700

    dump commit

--- scm/scripts/docker-entrypoint.sh
+++ scm/scripts/docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e
-
 # Run the migration first using the custom release task
 /opt/scm/bin/scm eval "SCM.ReleaseTasks.migrate"
 # Launch the OTP release and replace the caller as Process #1 in the container
